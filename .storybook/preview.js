327a09e54a197d6164d6ff510c67e9005234ea53
angelo <angelo.silveira09@gmail.com>
2022-09-27 09:23:20 -0300

    fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,6 +6,19 @@ import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 
 export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  },
   nextRouter: {
     Provider: RouterContext.Provider
   }
