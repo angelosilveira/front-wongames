e764e2b09afc58b4edb4135b7ef7a5acabd100c8
angelo <angelo.silveira09@gmail.com>
2022-09-30 19:05:00 -0300

    fix

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -30,7 +30,8 @@ export default {
     lightBg: '#F2F2F2',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
-    darkGray: '#2E2F42'
+    darkGray: '#2E2F42',
+    red: '#FF6347'
   },
   spacings: {
     xxsmall: '0.8rem',
